4f26161b4ae077905277aad921ac95c97ed89b24
Andrej Benjak <abenjak@gmail.com>
2021-10-13 21:22:24 +0200

    :rewind: remove non-coding from the R script

--- prepareHotspotVCFs.R
+++ prepareHotspotVCFs.R
@@ -11,19 +11,17 @@ library("optparse")
 # define argument list and data types
 option_list <- list(
   make_option(c("--mutations"), type="character", default = NULL, 
-              help="See help of main.nf", metavar="character"),
+              help="Mutation table", metavar="character"),
   make_option(c("--tumor_ID"), type="character", default = NULL, 
-               help="See help of main.nf", metavar="character"),
+               help="Used as prefix", metavar="character"),
   make_option(c("--hotspots"), type="character", default = NULL, 
-              help="See help of main.nf", metavar="character"),
-  make_option(c("--noncoding_hotspots"), type="character", default = NULL, 
-              help="See help of main.nf", metavar="character"),
+              help="Hotspot table (single amino acid hotspots)", metavar="character"),
   make_option(c("--splicesite_hotspots"), type="character", default = NULL, 
-              help="See help of main.nf", metavar="character"),
+              help="Splice hotspot table", metavar="character"),
   make_option(c("--indel_hotspots"), type="character", default = NULL,
-              help="See help of main.nf", metavar="character"),
+              help="Indel hotspot table", metavar="character"),
   make_option(c("--skip_3D"), type="character", default = "yes",
-              help="[yes|no] See help of main.nf", metavar="character")
+              help="[yes|no] Omit 3D hotspots", metavar="character")
 )
 
 # parse arguments
@@ -31,7 +29,7 @@ opt_parser <- OptionParser(option_list = option_list)
 opt        <- parse_args(opt_parser)
 
 # check if mandatory arguments were provided, show help menu otherwise
-if ( is.null(opt[["mutations"]]) | is.null(opt[["tumor_ID"]]) | is.null(opt[["hotspots"]]) | is.null(opt[["noncoding_hotspots"]]) | is.null(opt[["splicesite_hotspots"]]) | is.null(opt[["indel_hotspots"]]) ) {
+if ( is.null(opt[["mutations"]]) | is.null(opt[["tumor_ID"]]) | is.null(opt[["hotspots"]]) | is.null(opt[["splicesite_hotspots"]]) | is.null(opt[["indel_hotspots"]]) ) {
   print_help(opt_parser)
   print(opt)
   stop("One or more mandatory arguments missing.", call=FALSE)
@@ -101,34 +99,6 @@ if (length(rownames(muts[grepl("HOTSPOT", muts$hotspot),])) > 0) {
 } else {
   cat("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n", file = paste0(opt[["tumor_ID"]],"_hotspot.vcf"))
 }
-
-
-
-
-#############################
-### non-coding hotspots   ###
-#############################
-noncoding <- read.delim(as.character(opt[["noncoding_hotspots"]]), as.is = T)
-
-# if the mutation input has no 'chr' prefix, remove it from the hotspot lists
-if(!any(grepl("chr", muts$CHROM))){
-  noncoding$CHROM[which(grepl("chr", noncoding$CHROM))] <- gsub("chr", "",noncoding$CHROM[which(grepl("chr", noncoding$CHROM))])
-}
-
-# noncoding hotspots need to be an exact match of chr, pos and alt base. Just save the 'noncoding hotspots' rows with an exact match.
-noncoding$matchID <- paste(noncoding$CHROM,noncoding$POS,noncoding$REF,noncoding$ALT,sep=";")
-muts$matchID <- paste(muts$CHROM, muts$POS, muts$REF, muts$ALT,sep=";")
-
-if(any(muts$matchID %in% noncoding$matchID)){
-  vcf_hotspotNC <- noncoding[which(noncoding$matchID %in% muts$matchID),]
-  vcf_hotspotNC$FILTER <- "PASS"
-  vcf_hotspotNC <- vcf_hotspotNC[c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO")]
-  colnames(vcf_hotspotNC) <- c("#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO")
-  write.table(vcf_hotspotNC, file = paste0(opt[["tumor_ID"]],"_hotspot_noncoding.vcf"), row.names = F, quote = F, sep = '\t')
-} else {
-  cat("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n", file = paste0(opt[["tumor_ID"]],"_hotspot_noncoding.vcf"))
-}
-
 
 
 
